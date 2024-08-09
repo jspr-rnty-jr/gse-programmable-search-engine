00e3232a08ebaa3d12ae52699fe9d73b4cb540b6
jspr-rnty-jr <jasperjr@rantay@gmail.com>
2024-08-09 15:26:12 +0800

    modify preview image in large screen and add close button to hide the image prev

--- script.js
+++ script.js
@@ -1,30 +1,23 @@
-// const hideTagArea = () => {
-//     const tagArea = document.querySelector('.gsc-positioningWrapper');
-//     tagArea.style.display = 'none'
-// }
+const createCloseButton = () => {
+    const imgPrevLg = document.querySelector('.gs-imagePreviewArea');
+    const closeBtn = document.createElement("button");
+    closeBtn.classList.add('close-button');
+    closeBtn.innerText = "x";
+    imgPrevLg.appendChild(closeBtn);
 
-// const getPreviewImage = () => {
-//     let imagePrev = document.querySelector('.gs-imagePreviewArea')
-//     return imagePrev;
-// }
+    closeBtn.addEventListener('click', () =>{
+        imgPrevLg.style.display = 'none';
+    })
+}
 
-// const getImageResult = setTimeout(() => {
-//     let gscImageResults = document.querySelectorAll('.gsc-result');
-//     gscImageResults.forEach(result => {
-//         result.style.height = '100%'
-//         result.addEventListener('click', () => {
-//             let parentEl = result.parentNode
-//             let imageArea = getPreviewImage()
-//            if(imageArea){
-//                 imageArea.style.position = 'absolute'
-//                 imageArea.style.top = '3%'
-//                 imageArea.style.left = '50%'
-//                 imageArea.style.transform = 'translate(-50%,0%)'
-//                 imageArea.style.width = '75%'
-//                 parentEl.style.paddingTop = '400px'
-//            }
-//         })
-//     });
-// }, 1000);
-
-// setTimeout(() => hideTagArea(), 1000);
+const getImageResult = setTimeout(() => {
+    let gscImageResults = document.querySelectorAll('.gsc-result');
+    gscImageResults.forEach(result => {
+        result.addEventListener('click', () => {
+            const closeBtn = document.querySelector('.close-btn')
+            if(!closeBtn){
+                createCloseButton();
+            }
+        })
+    });
+}, 1000);
