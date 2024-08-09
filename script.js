93a08f01defc588f7051534b8ada15cd0b48099a
jspr-rnty-jr <jasperjr@rantay@gmail.com>
2024-08-09 14:43:37 +0800

    initial commmit

---
+++ script.js
@@ -0,0 +1,30 @@
+// const hideTagArea = () => {
+//     const tagArea = document.querySelector('.gsc-positioningWrapper');
+//     tagArea.style.display = 'none'
+// }
+
+// const getPreviewImage = () => {
+//     let imagePrev = document.querySelector('.gs-imagePreviewArea')
+//     return imagePrev;
+// }
+
+// const getImageResult = setTimeout(() => {
+//     let gscImageResults = document.querySelectorAll('.gsc-result');
+//     gscImageResults.forEach(result => {
+//         result.style.height = '100%'
+//         result.addEventListener('click', () => {
+//             let parentEl = result.parentNode
+//             let imageArea = getPreviewImage()
+//            if(imageArea){
+//                 imageArea.style.position = 'absolute'
+//                 imageArea.style.top = '3%'
+//                 imageArea.style.left = '50%'
+//                 imageArea.style.transform = 'translate(-50%,0%)'
+//                 imageArea.style.width = '75%'
+//                 parentEl.style.paddingTop = '400px'
+//            }
+//         })
+//     });
+// }, 1000);
+
+// setTimeout(() => hideTagArea(), 1000);
